6001ac878b438b686aaff960cdad934d1ac7f5e8
Maitreya Sinha <maitreya@live.in>
2026-05-14 13:38:57 +0200

    Initial Commit

---
+++ RewriteAI/RewriteAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewriteAIApp.swift
+//  RewriteAI
+//
+//  Created by Maitreya Sinha on 14/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct RewriteAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RewriteAITests/RewriteAITests.swift
@@ -0,0 +1,19 @@
+//
+//  RewriteAITests.swift
+//  RewriteAITests
+//
+//  Created by Maitreya Sinha on 14/05/2026.
+//
+
+import Testing
+@testable import RewriteAI
+
+struct RewriteAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
